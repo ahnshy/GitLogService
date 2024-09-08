88b59f365a7aa7f50598b70301e912ee30c5b003
ahnshy <ahnshy@naver.com>
2024-09-08 21:02:17 +0900

    <feat> : add RESTful Response MSDN sample

--- src/GitLogService/LogController.cs
+++ src/GitLogService/LogController.cs
@@ -1,12 +1,35 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Web.Http;
 
 namespace GitLogService
 {
     public class LogController : ApiController
     {
-        public string Get()
+        // GET api/values 
+        public IEnumerable<string> Get()
         {
-            return "log sample";
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values 
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
         }
     }
 }
