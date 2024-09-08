1c32eaaa0ad6087086e3df8802ee30330dc9bae0
ahnshy <ahnshy@naver.com>
2024-09-08 20:49:30 +0900

    <feat> : add LogController.cs

---
+++ src/GitLogService/LogController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace GitLogService
+{
+    public class LogController : ApiController
+    {
+        public string Get()
+        {
+            return "log sample";
+        }
+    }
+}
